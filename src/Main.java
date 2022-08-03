0201af50ca27a93892f4a08f6a81878362ddb494
PopovaElis <elizprest@gmail.com>
2022-08-03 09:13:18 +0500

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//задание 1
+public class Main {
+    public static void main(String[] args) {
+        //задание номер 1
+        int myAge = 27;
+        short a = -100;
+        byte b = -11;
+        long c = 111_111_111_111_111L;
+        float d = 1.11f;
+        double e = 11.11111111;
+        char f='#';
+        boolean g= b>a;
+    }
+}
