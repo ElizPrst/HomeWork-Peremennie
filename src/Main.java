55f4c8c852f2715129c1ba607f8309fd134de2bc
PopovaElis <elizprest@gmail.com>
2022-08-03 09:19:39 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-//задание 1
+// задание 2
 public class Main {
     public static void main(String[] args) {
-        //задание номер 1
-        int myAge = 27;
-        short a = -100;
-        byte b = -11;
-        long c = 111_111_111_111_111L;
-        float d = 1.11f;
-        double e = 11.11111111;
-        char f='#';
-        boolean g= b>a;
+        //задание номер 2
+        double boxer1=78.2;
+        double boxer2=82.7;
+        double sum=boxer1+boxer2;
+        double sub=boxer2-boxer1;
+        System.out.println("Сумма веса двух боксеров "+sum+"кг" );
+        System.out.println("Разница в весе между двумя боксерами "+sub+"кг");
     }
 }
